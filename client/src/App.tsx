aed2bc38ea89dc88bcadc46f1674642c9d1f5d8f
Jonathan Perapalanunt <jpapernotes@gmail.com>
2020-04-13 20:06:46 -0700

    Increment count for all clients

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,14 @@ type AppProps = {
 }
 
 type AppState = {
-  connections: number
+  connections: number,
   count: number
 }
 
+const socket = socketIOClient('http://localhost:3001/');
+
 class App extends Component<AppProps, AppState> {
+
   constructor(props: AppProps) {
     super(props);
     this.state = {
@@ -18,28 +21,19 @@ class App extends Component<AppProps, AppState> {
       count: 0
     }
   }
-  incrementConnections = () => {
-    this.setState({
-      connections: this.state.connections + 1
-    });
-  }
 
   incrementCount = () => {
-    this.setState({
-      count: (this.state.count + 1)
-    });
+    socket.emit('increment count', () => {});
   }
 
   componentDidMount() {
-    // TODO update connection value too all clients
-    const socket = socketIOClient('http://localhost:3001/');
-    socket.on('my_new_connection', () => this.incrementConnections());
+    // subscribe to the update count
+    socket.on('update count', (count: number) => this.setState({count}));
   }
 
   render() {
     return (
       <div>
-        <p>Connections: {this.state.connections}</p>
         <p>Count: {this.state.count}</p>
         <button onClick={this.incrementCount}>Increment Count</button>
       </div>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = 3001
 
+let count = 0
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello</h1>');
 });
@@ -10,9 +12,14 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('User connected');
 
-  socket.emit("my_new_connection", () => console.log("Emitting new connection"));
-  socket.on("disconnect", () => console.log("User disconnected"));
+  // data should be the new count number
+  socket.on('increment count', () => {
+    count += 1;
+    console.log("increment received");
+    io.emit("update count", count);
+  });
 
+  socket.on("disconnect", () => console.log("User disconnected"));
 });
 
 server.listen(port, () => {
